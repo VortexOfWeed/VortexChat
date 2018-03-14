e7eed9ae5a3fcf02d78fb57e80f89da95364d36a
nirup63 <nirup.accel03@gmail.com>
2018-03-14 12:41:02 +0530

    deploying empty server verticle

--- src/main/java/org/vortex/chat/App.java
+++ src/main/java/org/vortex/chat/App.java
@@ -5,14 +5,6 @@ import io.vertx.core.Vertx;
 public class App {
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-
-        vertx.deployVerticle(new RecevierVerticle());
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        vertx.deployVerticle(new BroadcastVerticle());
-
+        vertx.deployVerticle(new ServerVerticle());
     }
 }
